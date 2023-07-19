976296e6ee6c523f264b473f6d7463a483da1695
Vladimir <vladimirhaidalov@gmail.com>
2023-07-19 20:29:09 +0200

    555

---
+++ redis/my practice/my  second ai.py
@@ -0,0 +1,11 @@
+import numpy as np
+array=np.array ([1,2,3,4,5])
+mean_value = np.mean(array)
+sum_value = np.sum(array)
+max_value = np.max(array)
+print('Среднее значений: ' , mean_value)
+print('Сумма значений: ', sum_value)
+print('Максимальное значение: ', max_value)
+
+
+
