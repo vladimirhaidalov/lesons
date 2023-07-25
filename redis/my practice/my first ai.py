0c490d474521d62b1e93d9ced183adc7efcb2e14
Vladimir <vladimirhaidalov@gmail.com>
2023-07-25 19:23:57 +0200

    888

--- redis/my practice/my first ai.py
+++ redis/my practice/my first ai.py
@@ -1,31 +1,9 @@
-
-import tensorflow as tf
-from tensorflow.keras import layers
-
-# Создание модели
-model = tf.keras.Sequential([
-    layers.Dense(64, activation='relu', input_shape=(784,)),
-    layers.Dense(10, activation='softmax')
-])
-
-# Компиляция модели
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
-# Загрузка и подготовка данных (пример MNIST)
-mnist = tf.keras.datasets.mnist
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-train_images, test_images = train_images / 255.0, test_images / 255.0
-
-# Обучение модели
-model.fit(train_images, train_labels, epochs=5)
-
-# Оценка точности модели
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print("Точность на тестовых данных:", test_acc)
-
-
+from sklearn.datasets import load_iris
+import pandas as pd
+iris= load_iris()
+df =pd.DataFrame(data=iris.data, columns=iris.feature_names)
+df['target']=iris.target_names[iris.target]
+print(df.head())
 
 
 
---
+++ redis/my practice/my sixth project.py
@@ -0,0 +1,30 @@
+def calculate_bmi(weight,height):
+    bmi = weight/(height**2)
+    return bmi
+
+underweight='Недостаточный вес'
+normal_weight='нормальный вес'
+overweight='Избыточный вес'
+obesity='Ожирение'
+
+def interpret_bmi(bmi):
+    if bmi<18.5:
+        return underweight
+    elif 18.5 <= bmi< 24.9:
+        return normal_weight
+    elif 25<=bmi<29.9:
+        return overweight
+    else:
+        return obesity
+def main():
+    weight = float(input('Введите свой вес в килограммах: '))
+    height = float(input('Введите свой рост в метрах: '))
+    bmi = calculate_bmi( weight,height)
+    interpretation = interpret_bmi(bmi)
+    print("Ваш BMI: {:.2f}".format(bmi))
+    print('Интерпретация: {}'.format(interpretation))
+
+if __name__=="__main__":
+    main() 
+
+
